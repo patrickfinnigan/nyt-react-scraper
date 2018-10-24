92daa4150e82ccec742b20976d9f6a891079257c
patrickfinnigan <patrickfinnigan94@gmail.com>
2018-10-23 21:56:01 -0700

    file changes

--- server/index.js
+++ server/index.js
@@ -1,66 +1,68 @@
 // Test insert to DB
-const seedDb = require('./models/seed').seedDb;
+const seedDb = require("./models/seed").seedDb;
 // const readDb = require('./controllers/products').readProducts;
 
 // Initialize Express
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // Import routes
-const router = require('./router');
+const router = require("./router");
 
 // Import Node native modules
-const http = require('http');
-const path = require('path');
+const http = require("http");
+const path = require("path");
 
 // Import Mongoose
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import Middleware
-const morgan = require('morgan');  // Helps us develop and debug locally
-const cors = require('cors'); // Helps us bypass CORS issues when developing locally
-const bodyParser = require('body-parser'); // Helps us extract features from req.body
+const morgan = require("morgan"); // Helps us develop and debug locally
+const cors = require("cors"); // Helps us bypass CORS issues when developing locally
+const bodyParser = require("body-parser"); // Helps us extract features from req.body
 
 class App {
-    constructor() {
-        this.port = process.env.PORT || 3090;
-        this.server = http.createServer(app);
-        this.initDb();
-        this.initMiddleware();
-        this.run();
+  constructor() {
+    this.port = process.env.PORT || 3090;
+    this.server = http.createServer(app);
+    this.initDb();
+    this.initMiddleware();
+    this.run();
+  }
+  initDb() {
+    try {
+      this.db = mongoose.connect(
+        process.env.MONGODB_URI || "mongodb://localhost/nytscraper",
+        { useNewUrlParser: true } // Helps us avoid deprecation errors.
+      );
+      console.log("Successfully connected to database.");
+    } catch (error) {
+      console.log("Failed to connect to MongoDB.", error);
     }
-    initDb() {
-        try {
-            this.db = mongoose.connect(
-                process.env.MONGODB_URI || 'mongodb://localhost/nytscraper',
-                { useNewUrlParser: true } // Helps us avoid deprecation errors.
-            );
-            console.log('Successfully connected to database.')
-        } catch (error) {
-            console.log('Failed to connect to MongoDB.', error)
-        }
-    }
-    initMiddleware() {
-        app.use(cors());
-        app.use(bodyParser.json({ type: '*/*'})); // Type indicates ALL header types OK
-        app.use(express.static(path.resolve(__dirname, '..', '..', 'public'))) // Serve files in our Rect app public directory
-        router(app);
+  }
+  initMiddleware() {
+    app.use(cors());
+    app.use(bodyParser.json({ type: "*/*" })); // Type indicates ALL header types OK
+    // app.use(express.static(path.resolve(__dirname, '..', '..', 'public'))) // Serve files in our Rect app public directory
+    router(app);
 
-        if (process.env.NODE_ENV === 'production')
-            app.use(express.static('client/build'));
-        if (process.env.NODE_ENV === 'test')
-            app.use(morgan(() => null));
-        else
-            app.use(morgan('API Request (port ' + this.port + '): :method :url :status :response-time ms - :res[content-length]'));
-    }
-    run() {
-        this.server.listen(this.port, () => {
-            console.log(`Server listening on port ${this.port}`)
-        })
-    }
+    if (process.env.NODE_ENV === "production")
+      app.use(express.static("client/build"));
+    if (process.env.NODE_ENV === "test") app.use(morgan(() => null));
+    else
+      app.use(
+        morgan(
+          "API Request (port " +
+            this.port +
+            "): :method :url :status :response-time ms - :res[content-length]"
+        )
+      );
+  }
+  run() {
+    this.server.listen(this.port, () => {
+      console.log(`Server listening on port ${this.port}`);
+    });
+  }
 }
 
 new App();
-
-
-
--- server/router.js
+++ server/router.js
@@ -3,9 +3,9 @@ const db = require("./controllers/controller");
 
 module.exports = function(app) {
   // Ensures your react app is being served at all times
-  app.get("*", function(req, res, next) {
-    res.sendFile(__dirname, "../client/build/index.html");
-  });
+  // app.get("*", function(req, res, next) {
+  //   res.sendFile(__dirname, "../client/build/index.html");
+  // });
 
   app.post("/save", function(req, res, next) {
     db.save(req, res);
@@ -15,7 +15,7 @@ module.exports = function(app) {
     db.get_all(req, res);
   });
 
-  app.delete("/delete/:id", function (req, res, next) {
+  app.delete("/delete/:id", function(req, res, next) {
     db.delete(req, res);
   });
   // Wire up routes with calls to your database controllers here!
